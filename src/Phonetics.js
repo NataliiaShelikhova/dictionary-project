a38d228f7533a400c78a2a1fe79c528247bdd679
Nataliia Shelikhova <105817686+NataliiaShelikhova@users.noreply.github.com>
2022-10-03 20:39:43 +0300

    added react-audio-player

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -1,15 +1,20 @@
 import React from "react";
+import ReactAudioPlayer from 'react-audio-player';
 
 
 export default function Phonetics(props) {
+    function audioPlayer(event) {
+        event.preventDefault();
+        <ReactAudioPlayer src={props.phonetic.audio} autoplay />
+    }
     return (
         <div>
             <a href={props.phonetic.audio} target="_blank" rel="noreferrer noopener">
-                Listen {" "}
+                <img src="https://cdn-icons-png.flaticon.com/512/59/59284.png" alt="speaker-icon" width={15} className="icon " onClick={audioPlayer}/>
 
             </a>
           
-            {props.phonetic.text}
+           {" "} {props.phonetic.text}
         </div>
     )
 }
